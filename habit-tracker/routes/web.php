bf59b9609a8d56fbac9e011895f419d645c93561
GustavoCJesu <gustavo.jesuino1129@gmail.com>
2026-08-14 18:28:08 -0300

    Feat: Correções model usuarios

--- habit-tracker/routes/web.php
+++ habit-tracker/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
 
 
 Route::get('/', [SiteController::class, 'index'] );
+
+Route::get('/login', [LoginController::class, 'index']);
     
