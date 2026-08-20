3c8057ef79278f69576cabd8fd6f1a23d113291d
GustavoCJesu <gustavo.jesuino1129@gmail.com>
2026-08-20 08:57:20 -0300

    Realocação dos codigos php da pagina de contribuições

--- habit-tracker/app/Models/Habit.php
+++ habit-tracker/app/Models/Habit.php
@@ -30,4 +30,43 @@ class Habit extends Model{
         ->where('completed_at', Carbon::today()->toDateString())
         ->isNotEmpty();
     }
+
+    public static function generateYearGrid(int $year): array{
+
+
+        $startDate = Carbon::create($year, 1, 1);
+        $endDate   = Carbon::create($year, 12, 31, 23, 59, 59);
+
+
+        $weeks       = [];
+        $currentWeek = [];
+
+
+        $firstDayOfWeek = $startDate->dayOfWeek; // 0 = domingo, 1 = segunda, etc
+        for ($i = 0; $i < $firstDayOfWeek; $i++) {
+            $currentWeek[] = null; // Placeholder vazio
+        }
+
+
+        for ($date = $startDate->copy(); $date->lte($endDate); $date->addDay()) {
+            $currentWeek[] = $date->copy();
+
+            // Fecha a semana no sábado ou no último dia
+            if ($date->isSaturday() || $date->eq($endDate)) {
+                $weeks[]     = $currentWeek;
+                $currentWeek = [];
+            }
+        }
+
+        return $weeks;
+
+    }
+
+    public function wasCompletedOn(Carbon $date):bool {
+    
+
+        return $this->habitLog
+        ->where('completed_at', $date->toDateString())
+        ->isNotEmpty();
+    }
 }
